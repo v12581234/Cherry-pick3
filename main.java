79dd1196f6d3ce0308d4f1768f26270c301bc0c7
v12581234 <vinhnguyen125812@gmail.com>
2025-07-05 03:24:47 +0700

    Commit01: feature1 :first time

--- main.java
+++ main.java
@@ -3,4 +3,5 @@ public class main {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
